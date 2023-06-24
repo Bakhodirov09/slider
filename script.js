8d68885622b927e93805feb57be2db94d65eae61
Bakhodirov09 <bloghash01@gmail.com>
2023-06-24 16:42:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let imgBox = document.querySelector(".image-box")
+let btnOne = document.querySelector(".one")
+let btnTwo = document.querySelector(".two")
+let btnThree = document.querySelector(".three")
+let btnFour = document.querySelector(".four")
+
+btnOne.addEventListener("click" , function(){
+    imgBox.style.marginLeft = "0px"
+})
+
+btnTwo.addEventListener("click" , function(){
+    imgBox.style.marginLeft = "-600px"
+})
+
+btnThree.addEventListener("click" , function(){
+    imgBox.style.marginLeft = "-1200px"
+})
+
+btnFour.addEventListener("click" , function(){
+    imgBox.style.marginLeft = "-1800px"
+})
